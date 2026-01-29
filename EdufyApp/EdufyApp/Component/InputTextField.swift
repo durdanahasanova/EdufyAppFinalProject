2ffd032727fcf782e4af3c3fa770a419970ab71d
Durdana Hasan <durdanahasan08@gmail.com>
2026-01-29 09:07:45 -0800

    some changhes

---
+++ EdufyApp/EdufyApp/Component/InputTextField.swift
@@ -0,0 +1,18 @@
+//
+//  InputTextField.swift
+//  EdufyApp
+//
+//  Created by Durdana on 29.01.26.
+//
+
+import SwiftUI
+
+struct InputTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputTextField()
+}
